4c6072dcdaeb4c42d1fa9fe848e6c39c493b8d7b
Ivan Butrim <vbutrim@gmail.com>
2020-08-13 20:03:40 +0300

    fix style

--- src/main/java/com/vbutrim/bot/ConnectedUsersManager.java
+++ src/main/java/com/vbutrim/bot/ConnectedUsersManager.java
@@ -3,7 +3,6 @@ package com.vbutrim.bot;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class ConnectedUsersManager {
@@ -28,14 +27,16 @@ public class ConnectedUsersManager {
     }
 
     /**
-     * @throws NoSuchElementException if user hasn't started chat with bot
+     * @throws RuntimeException if user hasn't started chat with bot
      */
-    @SuppressWarnings("OptionalGetWithoutIsPresent")
     public void changeCityId(Chat chat, int cityId) {
-        connectedUsersRepository.save(
-                findUserByChatId(chat.getId())
-                        .get()
-                        .withCityId(cityId)
-        );
+
+        findUserByChatId(chat.getId())
+                .ifPresentOrElse(
+                        connectedUser ->
+                                connectedUsersRepository.save(connectedUser.withCityId(cityId)),
+                        () -> {
+                            throw new RuntimeException("there is no chat with id " + chat.getId());
+                        });
     }
 }
